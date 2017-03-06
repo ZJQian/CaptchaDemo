57fd8dd455a0f2d8251b6a04ff64e851e1c20700
ZJQian <2326274522@qq.com>
2017-03-06 14:41:39 +0800

    controller

--- CaptchaDemo/ViewController.swift
+++ CaptchaDemo/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var codeView = CaptchaView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        codeView = CaptchaView.init(frame: CGRect.init(x: 50, y: 100, width: 80, height: 35))
+        codeView.isRotation = true
+        view.addSubview(codeView)
+        codeView.refreshCode { (str) in
+            print(str)
+        }
+        
+        let tap = UITapGestureRecognizer.init(target: self, action: #selector(tapClick(_:)))
+        codeView.addGestureRecognizer(tap)
+        
     }
 
+    func tapClick(_ sender: UITapGestureRecognizer) {
+        
+        codeView.refreshCode { (str) in
+            print(str)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
